17d4941f96eec734aaa9f96db60c5b419e2e3bbb
Nikos Athanasakis <emidekol@gmail.com>
2017-05-28 20:03:06 +0300

    Started adding Wikipedia API

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 var locations = [{
-    label: 'Home',
-    lat: -31.563910,
-    lng: 147.154312
+    label: 'Acropolis of Athens',
+    lat: 37.971545,
+    lng: 23.725723
   },
   {
-    label: 'Work',
-    lat: -33.718234,
-    lng: 150.363181
+    label: 'Temple of Olympian Zeus',
+    lat: 37.969300,
+    lng: 23.733111
   }
 ];
-var map;
 
 function initMap() {
+  var map;
 
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 14,
     center: locations[0]
   });
 
@@ -36,14 +36,75 @@ function initMap() {
     });
     loc.marker.addListener('click', onMarkerClick);
   });
-  var infowindow = new google.maps.InfoWindow({
-    content: '<h1>Zelia</h1>'
-  });
+
+  var infowindow = new google.maps.InfoWindow();
+  // var infowindow = new google.maps.InfoWindow({
+  //   content: '<h1>Zelia</h1>'
+  //   // content: function () {
+  //   //
+  //   // }
+  // });
 
   function onMarkerClick() {
     var marker = this;
+
     animateMarker(marker);
+
+    infowindow.setContent(getWikipediaData(marker.title));
+    // infowindow.setContent('<h3>' + marker.title + '</h3>');
+    // console.log(marker);
     infowindow.open(map, marker);
+  }
+  // var playListURL = 'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=India&prop=revisions&rvprop=content&callback=?';
+  // http://en.wikipedia.org/w/api.php?format=json&action=query&titles=Acropolis of Athens&prop=revisions&rvprop=content
+  function getWikipediaData(place) {
+    var processedData;
+    $.ajax({
+      url: 'http://en.wikipedia.org/w/api.php',
+      data: {
+        action: 'parse',
+        // action: 'query',
+        // titles: place,
+        page: place,
+        // prop: 'revisions',
+        prop: 'text',
+        section: '0',
+        // rvprop: 'content',
+        // list: 'search',
+        format: 'json'
+      },
+      dataType: 'jsonp',
+      success: function (data) {
+        processedData = processData(data);
+      }
+    });
+    console.log(processedData);
+    return processedData;
+  }
+
+  function processData(data) {
+    var markup = data.parse.text["*"];
+    var blurb = $('<div></div>').html(markup);
+
+    // remove links as they will not work
+    blurb.find('a').each(function () {
+      $(this).replaceWith($(this).html());
+    });
+
+    // remove any references
+    blurb.find('sup').remove();
+
+    // remove cite error
+    blurb.find('.mw-ext-cite-error').remove();
+    // $('#article').html($(blurb).find('p'));
+
+    console.log(data);
+    console.log(blurb);
+    // $.each(result.query.pages, function (i, item) {
+    //   console.log(item.title);
+    // });
+    return $(blurb).find('p');
+
   }
 
   function animateMarker(marker) {
@@ -54,7 +115,6 @@ function initMap() {
     }, 2000);
   }
 }
-
 
 // Overall viewmodel for this screen, along with initial state
 function MyViewModel() {
@@ -74,7 +134,7 @@ function MyViewModel() {
   });
 
   self.onListItemClick = function (location) {
-    console.log(location.marker);
+    // console.log(location.marker);
     // location.marker.click();
     google.maps.event.trigger(location.marker, 'click', {});
     // console.log(123);
