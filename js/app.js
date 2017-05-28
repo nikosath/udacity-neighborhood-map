4c6242e368bc90482e2e1b849d53b5b07263cbf3
Nikos Athanasakis <emidekol@gmail.com>
2017-05-29 01:33:49 +0300

    Added Wikipedia API calls

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var locations = [{
     lng: 23.725723
   },
   {
-    label: 'Temple of Olympian Zeus',
+    label: 'Temple of Olympian Zeus, Athens',
     lat: 37.969300,
     lng: 23.733111
   }
@@ -32,12 +32,13 @@ function initMap() {
       // label: loc.label[0],
       title: loc.label,
       icon: 'http://maps.google.com/mapfiles/marker' + loc.label[0] + '.png',
-      map: map
+      map: map,
+      wikiDesc: null
     });
     loc.marker.addListener('click', onMarkerClick);
   });
 
-  var infowindow = new google.maps.InfoWindow();
+  var infowindow = new google.maps.InfoWindow({maxWidth: 300});
   // var infowindow = new google.maps.InfoWindow({
   //   content: '<h1>Zelia</h1>'
   //   // content: function () {
@@ -50,10 +51,45 @@ function initMap() {
 
     animateMarker(marker);
 
-    infowindow.setContent(getWikipediaData(marker.title));
+    if (marker.wikiDesc === null) {
+      infowindow.setContent('Loading...');
+      fetchWikiDesc(marker);
+
+    } else {
+      infowindow.setContent(marker.wikiDesc);
+
+    }
+
+    // setTimeout(function () {
+    //   infowindow.setContent(marker.wikiDesc + 'ready');
+    // }, 2000);
+    // infowindow.setContent(getWikipediaData(marker.title));
     // infowindow.setContent('<h3>' + marker.title + '</h3>');
     // console.log(marker);
     infowindow.open(map, marker);
+  }
+
+  function fetchWikiDesc(marker) {
+    $.ajax({
+      url: 'http://en.wikipedia.org/w/api.php',
+      data: {
+        action: 'parse',
+        page: marker.title,
+        prop: 'text',
+        section: '0',
+        format: 'json'
+        // action: 'query',
+        // titles: place,
+        // prop: 'revisions',
+        // rvprop: 'content',
+        // list: 'search',
+      },
+      dataType: 'jsonp',
+      success: function (data) {
+        marker.wikiDesc = processData(data);
+        infowindow.setContent(marker.wikiDesc);
+      }
+    });
   }
   // var playListURL = 'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=India&prop=revisions&rvprop=content&callback=?';
   // http://en.wikipedia.org/w/api.php?format=json&action=query&titles=Acropolis of Athens&prop=revisions&rvprop=content
@@ -98,12 +134,14 @@ function initMap() {
     blurb.find('.mw-ext-cite-error').remove();
     // $('#article').html($(blurb).find('p'));
 
-    console.log(data);
-    console.log(blurb);
+    // console.log(data);
+    // console.log(blurb);
     // $.each(result.query.pages, function (i, item) {
     //   console.log(item.title);
     // });
-    return $(blurb).find('p');
+    return $(blurb).find('p').html();
+    // return '<h1>return</h1>'
+    // return markup;
 
   }
 
@@ -139,25 +177,7 @@ function MyViewModel() {
     google.maps.event.trigger(location.marker, 'click', {});
     // console.log(123);
   };
-  // var self = this;
-  //
-  // // Non-editable catalog data - would come from the server
-  // self.availableMeals = [
-  //     { mealName: "Standard (sandwich)", price: 0 },
-  //     { mealName: "Premium (lobster)", price: 34.95 },
-  //     { mealName: "Ultimate (whole zebra)", price: 290 }
-  // ];
-  //
-  // // Editable data
-  // self.seats = ko.observableArray([
-  //     new SeatReservation("Steve", self.availableMeals[0]),
-  //     new SeatReservation("Bert", self.availableMeals[0])
-  // ]);
-  //
-  // // Operations
-  // self.addSeat = function() {
-  //     self.seats.push(new SeatReservation("", self.availableMeals[1]));
-  // }
+
 }
 
 ko.applyBindings(new MyViewModel());
